e994ec759f1233b7061199ddd3acd3c519f6d6b1
Lan-kdl <95589611+Lan-kdl@users.noreply.github.com>
2022-01-14 18:19:57 -0500

    Completed the Analysis

--- pypoll.py
+++ pypoll.py
@@ -60,8 +60,16 @@ for candidate_name in candidate_votes:
         winning_percentage = vote_percentage
         # Set the winning_candidate = to the candidate's name
         winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f'-------------------------\n'
+    f'Winner: {winning_candidate}\n'
+    f'Winning Vote Count: {winning_count:,}\n'
+    f'Winning Percentage: {winning_percentage:.1f}%\n'
+    f'-------------------------\n')
 # Print the winning candidate, vote count, and percentage to terminal 
-    print(f"{candidate_name} received {vote_percentage:.1f}% of the vote.")
+print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+print(winning_candidate_summary)
 
 
 # Create a file name variable to a direct or indirect path to a file
